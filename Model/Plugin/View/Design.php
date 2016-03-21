3a6e7f284efc51542f9c55c28de3fbe3f4dc900a
am-shopgo <ammar@shopgo.me>
2016-03-21 16:14:48 +0200

    Add a plugin to handle loading admin theme from DB

---
+++ Model/Plugin/View/Design.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Copyright © 2016 ShopGo. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace ShopGo\AdminThemeSwitcher\Model\Plugin\View;
+
+use Magento\Theme\Model\View\Design as ViewDesign;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\Area;
+
+/**
+ * View design plugin
+ */
+class Design
+{
+    /**
+     * Admin theme design configuration XML path
+     */
+    const XML_PATH_ADMIN_THEME_ID = 'design/theme/admin_theme_id';
+
+    /**
+     * @var ViewDesign
+     */
+    protected $_viewDesign;
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    protected $_scopeConfig;
+
+    /**
+     * @param ViewDesign $viewDesign
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        ViewDesign $viewDesign,
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->_viewDesign  = $viewDesign;
+        $this->_scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * Get admin theme
+     *
+     * @param ViewDesign $subject
+     * @param string|int $result
+     * @return string|int
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function afterGetConfigurationDesignTheme(ViewDesign $subject, $result)
+    {
+        if ($this->_viewDesign->getArea() == Area::AREA_ADMINHTML) {
+            $result = $this->_scopeConfig->getValue(
+                self::XML_PATH_ADMIN_THEME_ID,
+                ScopeConfigInterface::SCOPE_TYPE_DEFAULT
+            );
+        }
+
+        return $result;
+    }
+}
